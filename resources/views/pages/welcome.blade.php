609c16bba31edb7c80e70b89c5c91adea2219d8a
Mark Labrecque <mark.labrecque@gmail.com>
2016-10-14 22:24:36 -0700

    Completed lesson 5.

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,22 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-    </head>
-    <body>
-        @if (empty($people))
-            <div>There are no people</div>
-        @else
-            <ul>
-                <!-- unless directive means if(!) -->
-                @foreach ($people as $person)
-                    <li>{{ $person }}</li>
-                @endforeach
-            </ul>
-        @endif
+@extends('layout')
 
-    </body>
-</html>
+@section('content')
+    @if (empty($people))
+        <div>There are no people</div>
+    @else
+        <ul>
+            <!-- unless directive means if(!) -->
+            @foreach ($people as $person)
+                <li>{{ $person }}</li>
+            @endforeach
+        </ul>
+    @endif
+@stop
