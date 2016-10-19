85375b66fa148ddb7b157be9206b90307e26e69e
Mark Labrecque <mark.labrecque@gmail.com>
2016-10-19 08:03:28 -0700

    Completed lession 15 (previous mission lessons were demos without code to show)

---
+++ app/helpers.php
@@ -0,0 +1,4 @@
+<?php
+
+// Put any helper functions here, but ensure you load it in composer.json under autoload->files['file.location']
+// Also, when you change composer autoload config, you need to run `composer dump-
