c4d14ba5a60e7f7e18ed531e04bf15e852228c63
Mark Labrecque <mark.labrecque@gmail.com>
2016-10-19 08:52:26 -0700

    Added some references on how to access the Service Container items.

--- app/helpers.php
+++ app/helpers.php
@@ -1,4 +1,15 @@
 <?php
 
 // Put any helper functions here, but ensure you load it in composer.json under autoload->files['file.location']
-// Also, when you change composer autoload config, you need to run `composer dump-
+// Also, when you change composer autoload config, you need to run `composer dump-autoload`
+
+// To access services via the service provider layer, you have a number of options.
+function getFilesystemService() {
+	// $this->app->make('filesystem');
+	// - or -
+	// app('filesystem')
+	// - or -
+	// App::make('filesystem');
+	// - or -
+	return $this->app['filesystem'];
+}
